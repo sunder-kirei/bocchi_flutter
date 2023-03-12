9103d803a31d29e7bf010d635c6f4ee7694ae06a
Sunder Kumar <sunderkumar2506@gmail.com>
2023-03-12 09:23:59 +0530

    connection reset by peer bypassed

--- lib/helpers/animepahe_scrapper.dart
+++ lib/helpers/animepahe_scrapper.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart';
 import 'package:html/parser.dart' as html;
 
 class AnimeScrapper {
-  static const _baseUrl = "animepahe.com";
+  static const _baseUrl = "animepahe.ru";
   // static const _apiUrl = "anime-api-vnkr.onrender.com";
   static const _apiUrl = "relieved-cyan-tuxedo.cyclic.app";
   static const _headers = {
@@ -17,37 +17,42 @@ class AnimeScrapper {
     required String releasedYear,
     String season = "unknown",
   }) async {
-    final url = Uri.https(_baseUrl, "api", {"m": "search", "q": query});
-    final response = await get(url, headers: _headers);
-    final responseBody = json.decode(response.body)["data"] as List<dynamic>;
-    final searchList = responseBody.map((anime) {
-      return {
-        "animeTitle": anime["title"],
-        "animeId": anime["session"],
-        "animeImg": anime["poster"],
-        "totalEpisodes": anime["episodes"],
-        "type": anime["type"],
-        "status": anime["status"],
-        "season": anime["season"].toString().toLowerCase().trim(),
-        "year": anime["year"].toString(),
-        "score": anime["score"],
-      };
-    }).toList();
-    if (searchList.isEmpty) return "";
-    final foundAnime = searchList.firstWhere(
-      (element) {
-        if (season == "unknown") {
-          return element["year"].toString() == releasedYear;
-        }
-        return element["year"].toString() == releasedYear &&
-            element["season"].toString() == season;
-      },
-      orElse: () => searchList[0],
-    );
-    if (foundAnime["error"] != null) {
-      return "";
+    try {
+      final url = Uri.https(_baseUrl, "api", {"m": "search", "q": query});
+      final response = await get(url, headers: _headers);
+      final responseBody = json.decode(response.body)["data"] as List<dynamic>;
+      final searchList = responseBody.map((anime) {
+        return {
+          "animeTitle": anime["title"],
+          "animeId": anime["session"],
+          "animeImg": anime["poster"],
+          "totalEpisodes": anime["episodes"],
+          "type": anime["type"],
+          "status": anime["status"],
+          "season": anime["season"].toString().toLowerCase().trim(),
+          "year": anime["year"].toString(),
+          "score": anime["score"],
+        };
+      }).toList();
+      if (searchList.isEmpty) return "";
+      final foundAnime = searchList.firstWhere(
+        (element) {
+          if (season == "unknown") {
+            return element["year"].toString() == releasedYear;
+          }
+          return element["year"].toString() == releasedYear &&
+              element["season"].toString() == season;
+        },
+        orElse: () => searchList[0],
+      );
+      if (foundAnime["error"] != null) {
+        return "";
+      }
+      return foundAnime["animeId"];
+    } catch (err) {
+      print(err);
+      throw err;
     }
-    return foundAnime["animeId"];
   }
 
   static Future<List<Map<String, dynamic>>> fetchAnimepaheEpisodes({
@@ -88,47 +93,51 @@ class AnimeScrapper {
     required String episodeId,
   }) async {
     final url = Uri.https(_baseUrl, "/play/$animeId/$episodeId");
-    final response = await get(url);
-    final parsedResponse = html.parse(response.body);
-    final sourceList = parsedResponse
-        .getElementById(
-          "resolutionMenu",
-        )
-        ?.children
-        .map((elem) {
-      final attributes = elem.attributes;
-      return {
-        "referrer": attributes["data-src"].toString(),
-        "resolution": attributes["data-resolution"].toString(),
-        "audio": attributes["data-audio"].toString(),
-        "group": attributes["data-fansub"].toString(),
-      };
-    }).toList();
-    final streamInfoList =
-        parsedResponse.getElementById("pickDownload")?.children;
-    final referrerList = sourceList
-        ?.map(
-          (e) => {
-            "url": e["referrer"],
-            "audio": e["audio"],
-            "resolution": e["resolution"],
-          },
-        )
-        .toList();
-    final apiCall = Uri.https(_apiUrl, "/watch", {
-      "url": json.encode(referrerList),
-    });
-    final kwikUrl = json.decode((await get(apiCall)).body) as List<dynamic>;
-    int size = sourceList?.length as int;
-    for (int i = 0; i < size; i++) {
-      sourceList![i] = {
-        ...sourceList[i],
-        "url": kwikUrl.firstWhere((element) =>
-            element["audio"] == sourceList[i]["audio"] &&
-            element["resolution"] == sourceList[i]["resolution"])["url"],
-        "streamInfo": streamInfoList![i].firstChild?.text ?? "",
-      };
+    try {
+      final response = await get(url);
+      final parsedResponse = html.parse(response.body);
+      final sourceList = parsedResponse
+          .getElementById(
+            "resolutionMenu",
+          )
+          ?.children
+          .map((elem) {
+        final attributes = elem.attributes;
+        return {
+          "referrer": attributes["data-src"].toString(),
+          "resolution": attributes["data-resolution"].toString(),
+          "audio": attributes["data-audio"].toString(),
+          "group": attributes["data-fansub"].toString(),
+        };
+      }).toList();
+      final streamInfoList =
+          parsedResponse.getElementById("pickDownload")?.children;
+      final referrerList = sourceList
+          ?.map(
+            (e) => {
+              "url": e["referrer"],
+              "audio": e["audio"],
+              "resolution": e["resolution"],
+            },
+          )
+          .toList();
+      final apiCall = Uri.https(_apiUrl, "/watch", {
+        "url": json.encode(referrerList),
+      });
+      final kwikUrl = json.decode((await get(apiCall)).body) as List<dynamic>;
+      int size = sourceList?.length as int;
+      for (int i = 0; i < size; i++) {
+        sourceList![i] = {
+          ...sourceList[i],
+          "url": kwikUrl.firstWhere((element) =>
+              element["audio"] == sourceList[i]["audio"] &&
+              element["resolution"] == sourceList[i]["resolution"])["url"],
+          "streamInfo": streamInfoList![i].firstChild?.text ?? "",
+        };
+      }
+      return sourceList ?? [{}];
+    } catch (err) {
+      throw err;
     }
-    return sourceList ?? [{}];
   }
 }
